1de5d3b8a8cf02efbf82938d538d90f89164a277
elmiri younes <elmiri.younes@hotmail.com>
2022-09-20 11:43:27 +0200

    updtae

--- view/src/components/quoteRandom/QuoteRandom.tsx
+++ view/src/components/quoteRandom/QuoteRandom.tsx
@@ -37,7 +37,7 @@ const QuoteRandom: React.FC = () => {
   const generateQuote = async () => {
     // const response = await axios.get("http://localhost:5000/api/random");
     const response = await axios.get(
-      "http://ec2-34-217-129-53.us-west-2.compute.amazonaws.com/api/random"
+      "http://ec2-35-89-219-149.us-west-2.compute.amazonaws.com/api/random"
     );
     setQuoteRandomText(response.data.content);
     setQuoteRandomAuthor(response.data.author);
