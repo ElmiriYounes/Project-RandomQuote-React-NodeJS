68f02332cb2b059bfc2db4e8a9358c67beebfa74
elmiri younes <elmiri.younes@hotmail.com>
2022-09-20 22:03:32 +0200

    update

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const quoteRoute = require("./routes/quoteRandom");
-var cors = require('cors')
+var cors = require("cors");
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-const port = 5000;
+const port = process.env.PORT;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use("/api", quoteRoute);
 
