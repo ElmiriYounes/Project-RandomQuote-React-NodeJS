dd42397f42113abbec8a38f9f7f851b3218f16d7
ElmiriYounes <elmiri.younes@hotmail.com>
2022-11-13 21:55:51 +0100

    update

--- view/src/components/button/ButtonStyle.tsx
+++ view/src/components/button/ButtonStyle.tsx
@@ -1,7 +1,6 @@
-import styled, {keyframes} from 'styled-components'
+import styled, { keyframes } from "styled-components";
 
-
-const bounding = keyframes`
+const boundingKeyframes = keyframes`
   from {
     width: 100%;
     height: 100%;
@@ -16,53 +15,61 @@ const bounding = keyframes`
 `;
 
 export const ButtonWrapper = styled.div`
-    margin: 50px auto 0 auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  margin: 50px auto 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  position: relative;
+
+  &::before {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    content: "";
+    background-color: rgb(96, 217, 250);
+    -webkit-animation: bounding 4s 0s ease-in-out infinite;
+    animation: bounding 4s 0s ease-in-out infinite;
     border-radius: 50%;
-    width: 120px;
-    height: 120px;
-    position: relative;
+    z-index: 0;
+  }
 
-    &::before{
-        position: absolute;
-        height: 100%;
-        width: 100%;
-        content: '';
-        background-color: rgb(96,217,250);
-        animation: ${bounding} 4s 0s ease-in-out infinite;
-        -webkit-animation: ${bounding} 4s 0s ease-in-out infinite;
-        border-radius: 50%;
-        z-index: 0;
-    }
+  &::after {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    content: "";
+    background-color: rgb(96, 217, 250);
+    -webkit-animation: bounding 4s 2s ease-in-out infinite;
+    animation: bounding 4s 2s ease-in-out infinite;
+    border-radius: 50%;
+    z-index: 0;
+  }
 
-    &::after{
-        position: absolute;
-        height: 100%;
-        width: 100%;
-        content: '';
-        background-color: rgb(96,217,250);
-        animation: ${bounding} 4s 2s ease-in-out infinite;
-        -webkit-animation: ${bounding} 4s 2s ease-in-out infinite;
-        border-radius: 50%;
-        z-index: 0;
-    }
-`
+  @-webkit-keyframes bounding {
+    ${boundingKeyframes}
+  }
+
+  @keyframes bounding {
+    ${boundingKeyframes}
+  }
+`;
 
 export const ButtonComponent = styled.button`
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    background-color: rgb(22,22,22);
-    border: 0px solid black;
-    cursor: pointer;
-    display: block;
-    color: #ffffff;
-    position: relative;
-    z-index: 1;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: rgb(22, 22, 22);
+  border: 0px solid black;
+  cursor: pointer;
+  display: block;
+  color: rgb(96, 217, 250);
+  position: relative;
+  z-index: 1;
 
-    &:active{
-        transform: scale(.92);
-    }
-`
+  &:active {
+    transform: scale(0.92);
+  }
+`;
